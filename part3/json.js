6cc2de760f655f8e31a1ac7fcba98d36c5a3f76c
camposveronica <36063615+camposveronica@users.noreply.github.com>
2018-05-01 18:46:20 -0700

    Assignment09
    Assignment09

---
+++ part3/json.js
@@ -0,0 +1,27 @@
+{"team":[
+	{
+		"name":"Veronica",
+		"title":"CEO",
+		"bio":"With over 14 years of public accounting and business advisory service experience in the Tongas, Ecuador, Canada, Botswana and the US, Veronica is the most seasoned and diversed member of the Vecta Corp. team."
+	},
+	{
+		"name":"Kon",
+		"title":"Vice President",
+		"bio":"Kon is the Director of Development for Vecta Corp. KOn creates learning materials for Vecta Corp. as well as consulting for customers to integrate vSolutions into their production pipeline and business processes."
+	},
+	{
+		"name":"Julia",
+		"title":"Director of Operations",
+		"bio":"Julia joined Vecta Corp. in October 1999 as Vecta Corp's first Director of Operations. As such, he has overall human resources responsibility for Vecta Corp's operations worldwide."
+	},
+	{
+		"name":"Krupskaia",
+		"title":"Sales and Marketing Director",
+		"bio":"Krupskaia serves as the Sales and Marketing Director for Vecta Corp. In this role Krupskaia oversees Vecta Corp's marketing and corporate communications efforts worldwide."
+	},
+	{
+		"name":"Veronica",
+		"title":"Founder and CEO",
+		"bio":"While Wilbur is the founder and CEO of Vecta Corp, he is primarily known for being the pioneer and world leader of creating vSolutions."
+	}
+]}
